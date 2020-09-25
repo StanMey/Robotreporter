95866d205087301ae57d6cba17d8b163e4193271
StanMey <stan_1998@hotmail.com>
2020-09-25 14:51:37 +0200

    Added first drafts for testing

---
+++ articles_app/tests/test_models.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from .models import Stocks, Articles
+
+class StocksTestCase(TestCase):
+    def setUp(self):
+        pass
+
+
+class ArticlesTestCase(TestCase):
+    def setUp(self):
+        pass
