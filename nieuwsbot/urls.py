2af2ebc940cc1b953d8a0136b10020f9ce0262cc
StanMey <stan_1998@hotmail.com>
2020-11-12 12:40:01 +0100

    US42: added reset password to the site

--- nieuwsbot/urls.py
+++ nieuwsbot/urls.py
@@ -27,10 +27,17 @@ urlpatterns = [
     path('', app_views.home, name='home'),
     path('modules/', include('articles_app.urls')),
     path('data/', include('articles_app.urls_data')),
+
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+
     path('robots.txt/', app_views.robots_txt, name='robots_txt'),
     path('privacy/', app_views.privacy_statement, name="privacy_statement"),
     path('cookies/', app_views.cookie_statement, name="cookie_statement"),
