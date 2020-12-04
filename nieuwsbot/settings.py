05570ae07817b49c2687d6a4cb3f62fda5534d3a
StanMey <stan_1998@hotmail.com>
2020-12-04 12:47:21 +0100

    updated .env example and settings

--- nieuwsbot/settings.py
+++ nieuwsbot/settings.py
@@ -80,36 +80,16 @@ WSGI_APPLICATION = 'nieuwsbot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': config("DB_DEV_NAME"),
-            'USER': config("DB_DEV_USER"),
-            'PASSWORD': config("DB_DEV_PASSWORD"),
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': config("DB_PROD_NAME"),
-            'USER': config("DB_PROD_USER"),
-            'PASSWORD': config("DB_PROD_PASSWORD"),
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 
 # Password validation
