f84ff8ec1e267b6c2ac8f440628c99ea334a54b4
StanMey <stan_1998@hotmail.com>
2020-12-08 21:41:50 +0100

    US72: anonymous user can now access the 3 latest articles

--- articles_app/tests/test_render_views.py
+++ articles_app/tests/test_render_views.py
@@ -10,10 +10,11 @@ class ViewsTestCase(TestCase):
     """Here all the views are tested that return an actual page (the render function is returned).
 
     Args:
-        TestCase ([type]): [description]
+        TestCase (django.test.TestCase): A subclass of unittest.TestCase
     """
     def setUp(self):
-        """Add a user and a group for testing purposes."""
+        """Add a user and a group for testing purposes.
+        """
         # create permission group
         group_name = "view_only"
         self.group = Group(name=group_name)
@@ -50,19 +51,23 @@ class ViewsTestCase(TestCase):
                                                id=1)
 
     def tearDown(self):
+        """Removes all the SetUp
+        """
         self.user1.delete()
         self.user2.delete()
         self.group.delete()
 
     # home view page
     def test_get_home_view(self):
-        """The home page loads properly."""
+        """The home page loads properly.
+        """
         response = self.client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     # robots.txt page
     def test_get_robots_txt(self):
-        """The robots.txt loads properly."""
+        """The robots.txt loads properly.
+        """
         response = self.client.get("/robots.txt", follow=True)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response["content-type"], "text/plain")
@@ -71,19 +76,22 @@ class ViewsTestCase(TestCase):
 
     # privacy statement page
     def test_get_privacy_statement(self):
-        """The privacy statement page loads properly."""
+        """The privacy statement page loads properly.
+        """
         response = self.client.get("/privacy", follow=True)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     # cookie statement page
     def test_get_cookie_statement(self):
-        """The cookie statement page loads properly."""
+        """The cookie statement page loads properly.
+        """
         response = self.client.get("/cookies", follow=True)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     # moduleA view page
     def test_moduleA_view_user_cannot_access(self):
-        """The module A view is prohibited for not logged in users."""
+        """The module A view is prohibited for not logged in users.
+        """
         # user is not logged in
         response = self.client.get('/modules/moduleA/')
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
@@ -97,7 +105,8 @@ class ViewsTestCase(TestCase):
 
     # moduleB view page
     def test_moduleB_view_user_cannot_access(self):
-        """The module B view is prohibited for not logged in users."""
+        """The module B view is prohibited for not logged in users.
+        """
         # user is not logged in
         response = self.client.get('/modules/moduleB/')
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
@@ -111,7 +120,8 @@ class ViewsTestCase(TestCase):
 
     # moduleC view page
     def test_moduleC_view_user_cannot_access(self):
-        """The module C view is prohibited for not logged in users."""
+        """The module C view is prohibited for not logged in users.
+        """
         # user is not logged in
         response = self.client.get('/modules/moduleC/')
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
@@ -125,7 +135,8 @@ class ViewsTestCase(TestCase):
 
     # moduleD view page
     def test_moduleD_view_user_cannot_access(self):
-        """The module D view is prohibited for not logged in users."""
+        """The module D view is prohibited for not logged in users.
+        """
         # user is not logged in
         response = self.client.get('/modules/moduleD/')
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
@@ -153,7 +164,8 @@ class ViewsTestCase(TestCase):
 
     # single article page
     def test_article_view_user_cannot_access(self):
-        """The article view is prohibited for not logged in users."""
+        """The article view is prohibited for not logged in users.
+        """
         # user is not logged in
         response = self.client.get(f"/modules/articles/{self.article.id}")
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
@@ -163,4 +175,19 @@ class ViewsTestCase(TestCase):
         """
         # user is logged in
         response = self.client2.get(f"/modules/articles/{self.article.id}")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    # 3 articles page for not logged in user
+    def test_load_latest_articles(self):
+        """A not logged in user can access the 3 most recent articles.
+        """
+        # user is not logged in
+        response = self.client.get("/modules/latest/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_load_latest_single_article(self):
+        """A not logged in user can access an article.
+        """
+        # user can access a article
+        response = self.client.get("/modules/latest/1")
         self.assertEqual(response.status_code, HTTPStatus.OK)
