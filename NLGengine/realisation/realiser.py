9dc9a7ebba7e77e2f0ecada2642e13841c88ec00
StanMey <stan_1998@hotmail.com>
2020-12-16 17:25:21 +0100

    US75: added punctuation to a sentence

--- NLGengine/realisation/realiser.py
+++ NLGengine/realisation/realiser.py
@@ -1,4 +1,5 @@
 from NLGengine.realisation.date_message import DateMessage
+from NLGengine.realisation.templatefiller import TemplateFiller
 from NLGengine.article import Article
 import math
 
@@ -20,6 +21,23 @@ class Realiser:
         """[summary]
         """
         self.add_time_to_observs_and_paragraphs()
+        self.add_interpunction()
+
+    def add_interpunction(self):
+        """Adds interpunction to every sentence (. at the end and capital letter at the beginning).
+        """
+        for par in self.paragraphs:
+            for observ in par.observations:
+
+                # add a . after each sentence
+                observ.observation_new += "."
+
+                # add a capital letter at the beginning
+                first_word = observ.observation_new.split()[0]
+                # check if the first word is a number
+                if not represents_integer(first_word):
+                    # first word is not an integer, so add capital letter
+                    observ.observation_new = observ.observation_new[0].capitalize() + observ.observation_new[1:]
 
     def add_time_to_observs_and_paragraphs(self):
         """[summary]
@@ -58,3 +76,20 @@ class Realiser:
                 # set the current observation
                 curr_observ = observ
                 count += 1
+
+
+def represents_integer(s):
+    """Checks whether a String is an integer.
+    https://stackoverflow.com/questions/1265665/how-can-i-check-if-a-string-represents-an-int-without-using-try-except
+
+    Args:
+        s (str): The string to check
+
+    Returns:
+        bool: Returns whether the string can be written as an integer
+    """
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
