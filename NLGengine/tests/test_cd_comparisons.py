bd65716e6f9cb3572be45525126341c2baf67c6d
StanMey <stan_1998@hotmail.com>
2021-01-09 16:36:35 +0100

    Added tests for comparison.py

--- NLGengine/tests/test_cd_comparisons.py
+++ NLGengine/tests/test_cd_comparisons.py
@@ -3,10 +3,7 @@ import numpy as np
 
 from datetime import datetime
 from NLGengine.observation import Observation
-from NLGengine.content_determination.determinator import check_pattern, check_period, check_component, has_overlap, \
-    is_focus_sector, generate_smoothing_mean
-
-# new_date = datetime(year=2020, month=9, day=30).replace(hour=00, minute=00, second=00, microsecond=0)
+from NLGengine.content_determination.comparisons import check_pattern, check_period, check_component, has_overlap
 
 
 def test_check_pattern():
@@ -248,53 +245,7 @@ def test_has_overlap():
     assert not has_overlap(date3, date4, date5, date6)
 
 
-def test_is_focus_sector():
-    """Tests the is_focus_sector function.
+def test_get_recency():
+    """Tests the get_recency function
     """
-    date1 = datetime.now()
-    date2 = datetime.now()
-
-    focus_sectors = ["A", "B", "C"]
-
-    observ1 = Observation("Q", date1, date2, "pat", "A", "ind", 3.4, 1.0, "sentence", 5.0, dict())
-    observ2 = Observation("Q", date1, date2, "pat", "B", "ind", 3.4, 1.0, "sentence", 5.0, dict())
-    observ3 = Observation("Q", date1, date2, "pat", "sec", "ind", 3.4, 1.0, "sentence", 5.0, dict())
-
-    # observation holds a sector to focus on
-    assert is_focus_sector(focus_sectors, observ1)
-    assert is_focus_sector(focus_sectors, observ2)
-    # observation doesn't hold a sector to focus on
-    assert not is_focus_sector(focus_sectors, observ3)
-
-    # observation holds one or multiple sectors to focus on
-    meta = {
-        "sectors": ["A", "B", "D"]
-    }
-    observ4 = Observation("Q", date1, date2, "pat", "A", "ind", 3.4, 1.0, "sentence", 5.0, meta)
-    assert is_focus_sector(focus_sectors, observ4)
-
-    # observation doesn't hold one or multiple sectors to focus on
-    meta = {
-        "sectors": ["D", "E"]
-    }
-    observ5 = Observation("Q", date1, date2, "pat", "A", "ind", 3.4, 1.0, "sentence", 5.0, meta)
-    assert not is_focus_sector(focus_sectors, observ5)
-
-
-def test_generate_smoothing_mean():
-    """Tests the generate_smoothing_mean function
-    """
-    x = np.array([1.0])
-    y = generate_smoothing_mean(1)
-    assert len(x) == len(y)
-    assert x == y
-
-    x = generate_smoothing_mean(6)
-    y = np.array([0.16666667, 0.33333333, 0.5, 0.66666667, 0.83333333, 1.0])
-    assert len(x) == len(y)
-    assert all([round(a, 3) == round(b, 3) for a, b in zip(x, y)])
-
-    x = generate_smoothing_mean(8)
-    y = np.array([0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0])
-    assert len(x) == len(y)
-    assert all([round(a, 3) == round(b, 3) for a, b in zip(x, y)])
+    pass
